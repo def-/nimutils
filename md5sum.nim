e97cde616193a2092642a7150080a2a6ea1489bc
def <dennis@felsin9.de>
2015-01-03 14:26:37 +0100

    Working md5sum

--- md5sum.nim
+++ md5sum.nim
@@ -1,9 +1,5 @@
 import os, memfiles, md5
 
-# working with
-# dd if=/dev/urandom of=tmp bs=1k count=250k
-# not working with
-# dd if=/dev/urandom of=tmp bs=1k count=290k
 var
   c: MD5Context
   d: MD5Digest
@@ -18,10 +14,9 @@ for param in commandLineParams():
   except:
     opened = false
 
-  echo file.size
   c.md5Init()
   if opened:
     c.md5Update(cast[cstring](file.mem), file.size)
   c.md5Final(d)
 
-  echo($d, " ", param)
+  echo($d, "  ", param)
